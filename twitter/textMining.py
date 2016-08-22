fec1849d84dce8c462fa1c629d2b2e3c660eaaa2
Oswaldo Bayona <obayona@espol.edu.ec>
2016-08-21 22:17:18 -0500

    se calcula LDA

--- twitter/textMining.py
+++ twitter/textMining.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import time
 from pymongo import *
 import re
-dbName = "tw_chile_argentina_final";
+from TopicModeling import *#algorito LDA
+
+dbName = "tw_arg_usa";
 
 client = MongoClient('localhost', 27017)
 db = client['twitter_db']
@@ -30,7 +32,7 @@ class Document():
 		return texto
 
 documents=[]
-df = pd.read_csv("tw_chile_argentina_final.csv")
+df = pd.read_csv("tw_arg_usa.csv")
 
 df['hour']=df.apply(lambda x:time.strftime('%H', time.strptime(x['created_at'],'%a %b %d %H:%M:%S +0000 %Y')),axis=1)
 df['minute']=df.apply(lambda x:int(time.strftime('%M', time.strptime(x['created_at'],'%a %b %d %H:%M:%S +0000 %Y')))//10*10,axis=1)
@@ -41,7 +43,21 @@ for key, item in grouped_df:
 	document=Document(collection)
 	aux_df.apply(document.obtenerPedazoDocumento,axis=1)
 	documents.append(document.documentText)
+
+
+#clase que clacula LDA, 
+#tern score es el metodo que selecciona las palabras mas relevantes
+lda_wrapper = TopicModelingLDA(documents, "term_score")
+#recibe numero de topicos: le pongo el numero de documentos xd
+#recibe el numero de iteraciones: le pongo 10
+lda_wrapper.fit(len(documents), 10)
+
+#se obtienen las 10 palabras mas importantes de cada topico
+top10 = lda_wrapper.get_highest_scores()
+
+"""
 for i,doc in enumerate(documents):
-	print "******************DOCUMENTO NUMERO: ",i, " ********************"
-	print doc
+	print ("******************DOCUMENTO NUMERO: ",i, " ********************")
+	print (doc)
+"""
 
